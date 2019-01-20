0ee1b290f7227bdfee7e959987684edf5fa4d61e
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-01-20 12:23:01 -0300

    M

--- javascript.js
+++ javascript.js
@@ -1,93 +1,46 @@
-var middle_earth_danger = prompt('Is Middle earth in danger?', '');
-var someone_else = prompt('Can someone else deal with it?');
+var middle_earth_danger = prompt('Is Middle earth in danger?');
 
-
-
-if(middle_earth_danger == 'no') {
+switch (middle_earth_danger){
+    case 'no':
     document.write('Good!');
-} else if(middle_earth_danger == 'yes'){
-    prompt('Is Middle earth in danger?', '');
+    break;
 
-    if (someone_else == 'yes') {
-        document.write('Good!');
-    } else if(someone_else == 'yes'){
-        prompt('Is Middle earth in danger?', '');
+    case 'yes':
+        var someone_else = prompt('Can someone else deal with it?');
+        switch (someone_else){
+            case 'yes':
+            document.write('Good!');
+            break;
+
+            case 'no':
+            var hobbits = prompt('what about Hobbits?');
+            switch (hobbits){
+            case 'yes':
+            document.write('Proceed to find a suitable hobbit');
+
+            break;
+
+            case 'no':
+            var magic =prompt('does magic work?');
+                switch (magic){
+                    case 'yes':
+                    var alive = prompt('are you alive?');
+                        switch(alive){
+                            case ('yes'):
+                            document.write('Good!');
+                            break;
+
+                            case ('no'):
+                            document.write('resurrect!');
+                            break;
+                        }
+                    case ('no'):
+                    document.write('Call the eagles!');
+                    brake;
+                }
+
+            }
+        }
+
     }
 
-
-}
-
-/*
-var numero1;
-var numero2;
-numero1 = parseInt(numero1);
-numero2 = parseInt(numero2);
-nombre = prompt('Ingresa un nombre: ','');
-
-//console.log('Tu nombre es: ', nombre);
-//alert('Nombre recibido');
-
-//console.log(nombre);
-//document.write(nombre);
-
-numero1 = prompt('Ingresa un número: ','');
-//document.write('Número 1: ');
-//document.write(numero1);
-
-document.write('Número 1: ', numero1);
-
-document.write('<br/>');
-
-numero2 = prompt('Ingresa un segundo número: ', '')
-//document.write('Número 2: ');
-//document.write(numero2);
-
-document.write('Número 2: ', numero1);
-
-document.write('<br/>');
-var nombre;
-
-nombre = prompt('Ingresá tu nombre','');
-document.write('Hola ', nombre);
-
-var numero;
-numero = prompt('Ingresa un tercer número', '');
-
-if(numero>10){
-    document.write('El número es mayor a 10');
-}
-
-if(numero<10){
-    document.write('El número es menor a 10');
-}
-
-if(numero==10){
-    document.write('El es a 10');
-} else{
-    document.write('El número no es 10');
-}
-
-var numero1;
-var numero2;
-numero1 = prompt('Ingresa un numero',);
-numero2 = prompt('Ingresa un segundo número',);
-
-if(numero1 == numero2) {
-    document.write('son iguales');
-} else if(numero1>numero2){
-    document.write('El primer número es mayor');
-}else if(numero1<numero2){
-    document.write('El primer número es menor');
-}*/
-/*
-var password1;
-var password2;
-
-password1 = prompt('Ingrese su clave');
-password2 = prompt('Reingrese su clave');
-
-if (password1 == password2) {
-    document.write('Iniciar sesión');
-} else {
-    document.write('No va a iniciar sesión');
-}*/
