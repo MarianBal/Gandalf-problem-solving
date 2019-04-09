92202e447413786d7da8008114154b8042574f3a
Mariana Bálsamo <44822014+MarianBal@users.noreply.github.com>
2019-04-09 16:02:35 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,68 +1,258 @@
 
+var pregunta = document.getElementById('pregunta')
 
-var middle_earth_danger = prompt('Is Middle earth in danger?');
+var si = document.getElementById('si');
+var no = document.getElementById('no');
 
-switch (middle_earth_danger){
-    case 'no':
-    var alive =prompt('Are you alive?');
 
-    switch (alive){
-        case 'yes':
-        document.write('Good');
-        break;
+si.onclick = function(){
+    pregunta.textContent = 'Can someone else deal with it?';
 
-        case 'no':
-        document.write('Resurrect!')
-
-        break;
-    } break;
-
-    case 'yes':
-        var someone_else = prompt('Can someone else deal with it?');
-        switch (someone_else){
-            case 'yes':
-            alive =prompt('Are you alive?');
-
-            switch (alive){
-                case 'yes':
-                document.write('Good');
-                break;
-
-                case 'no':
-                document.write('Resurrect!')
-
-                break;
-            } break;
-
-            case 'no':
-            var hobbits = prompt('what about Hobbits?');
-            switch (hobbits){
-            case 'yes':
-            document.write('Proceed to find a suitable hobbit');
-            break;
-
-            case 'no':
-            var magic =prompt('does magic work?');
-                switch (magic){
-                    case 'yes':
-                    alive = prompt('are you alive?');
-                        switch(alive){
-                            case ('yes'):
-                            document.write('Good!');
-                            break;
-
-                            case ('no'):
-                            document.write('Resurrect!');
-                            break;
-                        } break;
-                    case 'no':
-                    document.write ('Call the eagles!');
-                    break;
-
+    si.onclick = function(){
+        pregunta.textContent = 'Good! Are you alive?'
+    
+        si.onclick = function(){
+            pregunta.textContent = 'Are you in danger?'
+    
+            si.onclick = function(){
+                pregunta.textContent = 'Are swords of use here?'
+    
+                si.onclick = function(){
+    
+                    no.onclick = function(){
+                        return pregunta.textContent = ''
+                    }
+    
+                    return pregunta.textContent = 'Great!'
                 }
+                no.onclick = function(){
+                    pregunta.textContent = 'Does magic works?'
+    
+                    si.onclick = function(){
+                        no.onclick = function(){
+                            return pregunta.textContent = ''
+                        }
+                        return pregunta.textContent = 'Great!'
+                    }
+                    no.onclick = function(){
+    
+                        si.onclick = function(){
+                            return pregunta.textContent = ''
+                        }
+    
+                        return pregunta.textContent = 'Call the eagles!'
+                    }
+    
+    
+                }
+            }
+    
+            no.onclick = function(){
+    
+                si.onclick = function(){
+                    return pregunta.textContent = ''
+                }
+                return pregunta.textContent = 'Great!'
+            }
+        }
+    
+        no.onclick = function(){
+    
+            si.onclick = function(){
+                return pregunta.textContent = ''
+            }
+    
+            return pregunta.textContent = 'Resurrect'
+        }
+    }
+
+    no.onclick = function(){
+        pregunta.textContent = 'what about Hobbits?'
+
+        si.onclick = function(){
+            pregunta.textContent = 'Good! Are you alive?'
+        
+            si.onclick = function(){
+                pregunta.textContent = 'Are you in danger?'
+        
+                si.onclick = function(){
+                    pregunta.textContent = 'Are swords of use here?'
+        
+                    si.onclick = function(){
+        
+                        no.onclick = function(){
+                            return pregunta.textContent = ''
+                        }
+        
+                        return pregunta.textContent = 'Great!'
+                    }
+                    no.onclick = function(){
+                        pregunta.textContent = 'Does magic works?'
+        
+                        si.onclick = function(){
+                            no.onclick = function(){
+                                return pregunta.textContent = ''
+                            }
+                            return pregunta.textContent = 'Great!'
+                        }
+                        no.onclick = function(){
+        
+                            si.onclick = function(){
+                                return pregunta.textContent = ''
+                            }
+        
+                            return pregunta.textContent = 'Call the eagles!'
+                        }
+        
+        
+                    }
+                }
+        
+                no.onclick = function(){
+        
+                    si.onclick = function(){
+                        return pregunta.textContent = ''
+                    }
+                    return pregunta.textContent = 'Great!'
+                }
+            }
+        
+            no.onclick = function(){
+        
+                si.onclick = function(){
+                    return pregunta.textContent = ''
+                }
+        
+                return pregunta.textContent = 'Resurrect'
+            }
+        }
+
+        no.onclick = function(){
+            pregunta.textContent = 'Does magic work?'
+
+            si.onclick = function(){
+                pregunta.textContent = 'Good! Are you alive?'
+            
+                si.onclick = function(){
+                    pregunta.textContent = 'Are you in danger?'
+            
+                    si.onclick = function(){
+                        pregunta.textContent = 'Are swords of use here?'
+            
+                        si.onclick = function(){
+            
+                            no.onclick = function(){
+                                return pregunta.textContent = ''
+                            }
+            
+                            return pregunta.textContent = 'Great!'
+                        }
+                        no.onclick = function(){
+                            pregunta.textContent = 'Does magic works?'
+            
+                            si.onclick = function(){
+                                no.onclick = function(){
+                                    return pregunta.textContent = ''
+                                }
+                                return pregunta.textContent = 'Great!'
+                            }
+                            no.onclick = function(){
+            
+                                si.onclick = function(){
+                                    return pregunta.textContent = ''
+                                }
+            
+                                return pregunta.textContent = 'Call the eagles!'
+                            }
+            
+            
+                        }
+                    }
+            
+                    no.onclick = function(){
+            
+                        si.onclick = function(){
+                            return pregunta.textContent = ''
+                        }
+                        return pregunta.textContent = 'Great!'
+                    }
+                }
+            
+                no.onclick = function(){
+            
+                    si.onclick = function(){
+                        return pregunta.textContent = ''
+                    }
+            
+                    return pregunta.textContent = 'Resurrect'
+                }
+            }
+
+            no.onclick =function(){
+                si.onclick = function(){
+                    return pregunta.textContent = ''
+                }
+
+                pregunta.textContent = 'Call the eagles'
+            }
+
+        }
+    }
+}
+no.onclick = function(){
+    pregunta.textContent = 'Are you alive?'
+
+    si.onclick = function(){
+        pregunta.textContent = 'Are you in danger?'
+
+        si.onclick = function(){
+            pregunta.textContent = 'Are swords of use here?'
+
+            si.onclick = function(){
+
+                no.onclick = function(){
+                    return pregunta.textContent = ''
+                }
+
+                return pregunta.textContent = 'Great!'
+            }
+            no.onclick = function(){
+                pregunta.textContent = 'Does magic works?'
+
+                si.onclick = function(){
+                    no.onclick = function(){
+                        return pregunta.textContent = ''
+                    }
+                    return pregunta.textContent = 'Great!'
+                }
+                no.onclick = function(){
+
+                    si.onclick = function(){
+                        return pregunta.textContent = ''
+                    }
+
+                    return pregunta.textContent = 'Call the eagles!'
+                }
+
 
             }
         }
 
+        no.onclick = function(){
+
+            si.onclick = function(){
+                return pregunta.textContent = ''
+            }
+            return pregunta.textContent = 'Great!'
+        }
     }
 
+    no.onclick = function(){
+
+        si.onclick = function(){
+            return pregunta.textContent = ''
+        }
+
+        return pregunta.textContent = 'Resurrect'
+    }
+}
